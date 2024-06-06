10ee50b40be5a533ed08e2e29a32034b9324fecc
LiZixin <83320082+archlizix@users.noreply.github.com>
2024-06-06 23:44:33 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,29 +6,6 @@ use model_sys::SystemModel;
 use sftpm::{Cli, Commands};
 
 fn main() {
-    let system_model = SystemModel {
-        id: "example_id".to_string(),
-        host: "example_host".to_string(),
-        port: SystemModel::SSH_PORT_DEFAULT,
-        user: "example_user".to_string(),
-        mount_opts: vec!["option1".to_string(), "option2".to_string()],
-        mount_point: "/example/mount/point".to_string(),
-        auth_method: SystemModel::AUTH_METHOD_PUBLIC_KEY.to_string(),
-        ssh_key: Some("~/path/to/ssh/key".to_string()),
-        cmd_before_mount: "true".to_string(),
-    };
-
-    let (is_valid, errors) = system_model.validate();
-    if is_valid {
-        println!("SystemModel is valid.");
-        system_model.save("./config").unwrap();
-    } else {
-        println!("SystemModel is invalid:");
-        for (field, error) in errors {
-            println!("{}: {}", field, error);
-        }
-    }
-
     let env = EnvironmentModel::new();
 
     println!("{:?}", env); // 打印EnvironmentModel实例以验证其创建
@@ -70,6 +47,27 @@ fn main() {
         }) => {
             println!("input setup, id:{}, host:{}, port:{}, user:{}, mount_opt:{}, mount_point:{}, auth_method:{}, ssh_key:{}, cmd_before_mount:{}",
                 id, host, port, user, mount_opt, mount_point, auth_method, ssh_key, cmd_before_mount);
+            let system_model = SystemModel {
+                id: id.to_string(),
+                host: host.to_string(),
+                port: *port as u16,
+                user: user.to_string(),
+                mount_opts: vec!(mount_opt.to_string()),
+                mount_point: mount_point.to_string(),
+                auth_method: auth_method.to_string(),
+                ssh_key: Some(ssh_key.to_string()),
+                cmd_before_mount: cmd_before_mount.to_string(),
+            };
+            let (is_valid, errors) = system_model.validate();
+            if is_valid {
+                println!("SystemModel is valid.");
+                system_model.save("./config").unwrap();
+            } else {
+                println!("SystemModel is invalid:");
+                for (field, error) in errors {
+                    println!("{}: {}", field, error);
+                }
+            }
         }
         None => {}
     }
